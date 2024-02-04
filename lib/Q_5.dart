3e7e1d1a9f5add94b90b20b4600bc1a85233551b
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-02-04 19:50:50 +0530

    04-02-2024

---
+++ lib/Q_5.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(const app());
+}
+class app extends StatefulWidget {
+  const app({super.key});
+
+  @override
+  State<app> createState() => _appState();
+}
+
+class _appState extends State<app> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
